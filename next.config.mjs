caadc47b86814086fae336ac13241d28b3ad22f4
edayot <pro.e.dayot@gmail.com>
2024-08-28 09:55:04 +0200

    retry

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import next from 'next';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-nextConfig.distDir = 'build';
+// nextConfig.distDir = 'build';
 // nextConfig.output = "export";
 
 export default nextConfig;

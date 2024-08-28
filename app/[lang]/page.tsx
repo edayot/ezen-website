6b272af686dbf8c15b21cae6c1af6d42f612039d
edayot <pro.e.dayot@gmail.com>
2024-08-28 12:04:00 +0200

    fix types

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
-
 import { getDictionary } from '@/dictionaries/dictionaries'
 
+interface HomeProps {
+  params: {
+    lang: string;
+  };
+}
 
-export default async function Home({ params: { lang } }) {
-  const dict = await getDictionary(lang)
-  console.log("dict", dict)
+export default async function Home({ params: { lang } }: HomeProps) {
+  const dict = await getDictionary(lang);
+  console.log("dict", dict);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>

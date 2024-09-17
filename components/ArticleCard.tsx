3857c8f8f95426da17175749d737f23eb8a441f2
edayot <pro.e.dayot@gmail.com>
2024-09-17 12:00:27 +0200

    feat: search bar

--- components/ArticleCard.tsx
+++ components/ArticleCard.tsx
@@ -157,6 +157,7 @@ function throttle(mainFunction: (...args: any[]) => void, delay: number) {
 
 // Implement Infinite scroll
 export function ArticlesViewer({ dict, lang }: { lang: (typeof locales)[number], dict: any }) {
+  const [search, setSearch] = useState('');
   const [elements_data, set_elements_data] = useState<{ data: PlantData, id: string }[]>([]);
   const [date, setDate] = useState(0);
   const [infiniteScrollEnabled, setInfiniteScrollEnabled] = useState(true);
@@ -222,7 +223,13 @@ export function ArticlesViewer({ dict, lang }: { lang: (typeof locales)[number],
     };
   }, [infiniteScroll]);
 
-  const elements = elements_data.map(({ data, id }: { data: PlantData, id: string }) => (
+  const elements = elements_data.filter((ele) => {
+      if (search === '') return true;
+      return (
+        ele.data[lang].name.toLowerCase().includes(search.toLowerCase()) || 
+        ele.data.latin_name.toLowerCase().includes(search.toLowerCase())
+      );
+    }).map(({ data, id }: { data: PlantData, id: string }) => (
     <Element
       key={data.date}
       data={data}
@@ -240,6 +247,40 @@ export function ArticlesViewer({ dict, lang }: { lang: (typeof locales)[number],
         <div className="w-full h-full flex flex-row justify-end items-center">
           <NewArticle />
         </div>
+      </div>
+      <div className="w-1/4">
+        <Input
+          value={search}
+          onValueChange={setSearch}
+          label="Search"
+          isClearable
+          radius="lg"
+          classNames={{
+            label: "text-black/50 dark:text-white/90",
+            input: [
+              "bg-transparent",
+              "text-black/90 dark:text-white/90",
+              "placeholder:text-default-700/50 dark:placeholder:text-white/60",
+            ],
+            innerWrapper: "bg-transparent",
+            inputWrapper: [
+              "shadow-xl",
+              "bg-default-200/50",
+              "dark:bg-default/60",
+              "backdrop-blur-xl",
+              "backdrop-saturate-200",
+              "hover:bg-default-200/70",
+              "dark:hover:bg-default/70",
+              "group-data-[focus=true]:bg-default-200/50",
+              "dark:group-data-[focus=true]:bg-default/60",
+              "!cursor-text",
+            ],
+          }}
+          placeholder="Type to search..."
+          startContent={
+            <FiSearch className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />
+          }
+        />
       </div>
       <div className="flex gap-4 flex-wrap content-start items-center justify-center">
         {elements}

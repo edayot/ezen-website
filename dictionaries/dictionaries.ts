83273f41107413851f0534dd7f6a34e9e4987895
edayot <pro.e.dayot@gmail.com>
2024-09-18 11:37:49 +0200

    fix: rollback lang import

--- dictionaries/dictionaries.ts
+++ dictionaries/dictionaries.ts
@@ -2,14 +2,6 @@ import "server-only";
 
 import { defaultLocale, locales } from "@/langs";
 
-import {readFile} from 'fs/promises'
-import JSON5 from 'json5'
-
-async function loadJSON5(filename: string) {
-  const json5 = await readFile(process.cwd() + filename, 'utf8')
-  return JSON5.parse(json5)
-}
-
 export interface HomeProps {
   lang: (typeof locales)[number];
   name: string;
@@ -18,10 +10,14 @@ export interface HomeProps {
 
 function getDictionaryInternal(locale: string) {
   try {
-    return loadJSON5(`/dictionaries/${locale}.jsonc`)
+    return import(`@/dictionaries/${locale}.json`).then(
+      (module) => module.default,
+    );
   } catch (e) {
     console.warn(`File ${locale} not found, falling back to ${defaultLocale}`);
-    return loadJSON5(`/dictionaries/${defaultLocale}.json`)
+    return import(`@/dictionaries/${defaultLocale}.json`).then(
+      (module) => module.default,
+    );
   }
 }
 

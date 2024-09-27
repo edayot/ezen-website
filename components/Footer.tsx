65791062ad28addf5e0dd44c0e56b30a56c6c4b7
edayot <pro.e.dayot@gmail.com>
2024-09-27 13:20:59 +0200

    feat: couleurs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,23 +8,25 @@ export function Footer({}) {
   const t = useTranslation();
 
   return (
-    <div className="w-full bg-default/80">
-      <div className="flex flex-row justify-between items-center m-[0.15rem]">
-        <div className="flex flex-row justify-start text-[12px]">
-          {t["footer.madeBy"]}
-        </div>
+    <div className="w-full z-1 ">
+      <div className="bg-default/60 backdrop-blur-sm border-default/60 border">
+        <div className="flex flex-row justify-between items-center my-[0.15rem] mx-16">
+          <div className="flex flex-row justify-start text-[12px]">
+            {t["footer.madeBy"]}
+          </div>
 
-        <div className="flex flex-row justify-end mx-2 my-1">
-          <div className="">
-            <Link href="https://github.com/edayot/ezen-website">
-              <motion.button
-                whileHover={{ scale: 1.025 }}
-                whileTap={{ scale: 0.95 }}
-                className="w-full h-full"
-              >
-                <FiGithub size={16} />
-              </motion.button>
-            </Link>
+          <div className="flex flex-row justify-end mx-2 my-1">
+            <div className="">
+              <Link href="https://github.com/edayot/ezen-website">
+                <motion.button
+                  whileHover={{ scale: 1.025 }}
+                  whileTap={{ scale: 0.95 }}
+                  className="w-full h-full"
+                >
+                  <FiGithub size={16} />
+                </motion.button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>

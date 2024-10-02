2daced3523ecf84f7156bd5284a7d74df6f5a78f
edayot <pro.e.dayot@gmail.com>
2024-10-02 13:04:11 +0200

    feat: fix layout

--- components/ExportButton.tsx
+++ components/ExportButton.tsx
@@ -10,53 +10,69 @@ import { useTranslation } from "@/dictionaries/client";
 import { locales } from "@/utils/langs";
 import Image from "next/image";
 
-// Define the QR code component with a forwardRef
-const ComponentToPrint = React.forwardRef((props: {url: string, data: PlantData}, ref: any) => {
-  const t = useTranslation();
 
+
+
+function Article({data}: {data: PlantData}) {
+  return (<>
+    <p className="text-sm text-black font-sans italic">
+      {data.latin_name}
+    </p>
+    <p className="text-xl text-black font-sans font-bold">
+      {data.it.name}
+    </p>
+    <p className="text-sm text-black font-sans">
+      {data.it.desc.split("\n").map((line, index) => (
+        <span key={index}>
+          {line}
+          <br />
+        </span>
+      )
+      )}
+    </p>
+    </>)
+}
+
+function BottomQRCode({url}: {url: string}) {
   const fullArticle = {
     fr: "Article en entier sur :",
     en: "Full article on :",
     it: "Articolo completo su :",
   }
+  return (<>
+  <div className="flex flex-row justify-between w-full h-full items-center">
+    <div className="flex flex-col w-full h-full justify-around gap-4">
+      {locales.map((lang) => (
+        <div className="flex flex-row gap-2 items-center" key={lang}>
+          {langToFlag[lang]}
+          <p className="text-sm text-black font-sans">
+            {fullArticle[lang]}
+          </p>
+        </div>
+      ))}
+    </div>
+    <div className="flex flex-row justify-end items-end w-full">
+      <QRCode value={url} logoImage="/favicon.ico" size={128}/>
+    </div>
+  </div>
+  </>)
+}
+
+
+// Define the QR code component with a forwardRef
+const ComponentToPrint = React.forwardRef((props: {url: string, data: PlantData}, ref: any) => {
+  const t = useTranslation();
 
   return (
   <div ref={ref}>
-    <div className="bg-white aspect-[3/4] w-[34rem]">
-      <div className="flex flex-col justify-between gap-2">
-        <div className="m-12">
-          <p className="text-sm text-black font-sans italic">
-            {props.data.latin_name}
-          </p>
-          <p className="text-xl text-black font-sans font-bold">
-            {props.data.it.name}
-          </p>
-          <p className="text-sm text-black font-sans">
-            {props.data.it.desc.split("\n").map((line, index) => (
-              <span key={index}>
-                {line}
-                <br />
-              </span>
-            )
-            )}
-          </p>
-        </div>
-        <div className="m-12">
-          <div className="flex flex-row justify-between w-full h-full items-center">
-            <div className="flex flex-col w-full h-full justify-around gap-4">
-              {locales.map((lang) => (
-                <div className="flex flex-row gap-2" key={lang}>
-                  {langToFlag[lang]}
-                  <p className="text-sm text-black font-sans">
-                    {fullArticle[lang]}
-                  </p>
-                </div>
-              ))}
-            </div>
-            <div className="flex flex-row justify-end items-end w-full">
-              <QRCode value={props.url} logoImage="/favicon.ico" size={128}/>
-            </div>
-          </div>
+    <div className="bg-white w-[36rem] h-[46rem] p-16">
+      <div className="flex flex-col justify-between gap-2 h-full">
+      <div className="flex-grow overflow-hidden relative">
+        <Article data={props.data}/>
+        <div className="absolute bottom-0 left-0 right-0 h-8 bg-gradient-to-t from-white to-transparent pointer-events-none"></div>
+      </div>
+        <div className="mt-auto">
+          <BottomQRCode url={props.url}/>
         </div>
       </div>
     </div>
@@ -79,7 +95,7 @@ export function ExportButton({ id , data }: { id: string, data: PlantData }) {
   return (
     <>
       {/* Display the QRCode only when exporting */}
-      <div style={{ position: "absolute", left: "-10000px", top: "100px" }}>
+      <div style={{ position: "absolute", left: "100px", top: "100px" }}>
         <ComponentToPrint ref={qrRef} url={url} data={data} />
       </div>
 

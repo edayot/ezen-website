87a5a2a0b74b910fe8219d554772b8d61389a2d4
edayot <pro.e.dayot@gmail.com>
2024-10-07 12:30:44 +0200

    feat: google

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html>
       <head></head>
+      <meta name="google-site-verification" content="jD6OnB_lklNZ7DZCON3p3WD8KhE0CxlpeXLvZ_f_pV0" />
       <body className={inter.className}>
         <NextThemesProvider attribute="class" defaultTheme="dark">
           <ToastContainer />

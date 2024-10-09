850ebe301f1363487fbe4a99206257b4e2beeefa
edayot <pro.e.dayot@gmail.com>
2024-10-09 10:28:32 +0200

    feat: table

--- app/[lang]/auth/account/page.tsx
+++ app/[lang]/auth/account/page.tsx
@@ -1,158 +1,185 @@
 "use client";
+import { RenderFooter } from "@/components/Footer";
 import { UploadToCloud } from "@/components/FormEditor";
 import { IsUserLoggedIn, RedirectComponent } from "@/components/RedirectButton";
 import { useTranslation } from "@/dictionaries/client";
 import { HomeProps } from "@/dictionaries/dictionaries";
-import { auth, mapRef, signOutGlobal } from "@/utils/firebase";
-import { Button, Image, Input, Snippet } from "@nextui-org/react";
+import { auth, footerRef, mapRef, signOutGlobal } from "@/utils/firebase";
+import { FooterData } from "@/utils/footer";
+import { locales } from "@/utils/langs";
+import { addDoc, doc, getDocs, setDoc } from "@firebase/firestore";
+import {
+  Button,
+  Card,
+  CardBody,
+  Divider,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Image,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalHeader,
+  Snippet,
+  Tooltip,
+  useDisclosure,
+} from "@nextui-org/react";
 import { getDownloadURL } from "firebase/storage";
 import { redirect } from "next/navigation";
 import { useEffect, useState } from "react";
-import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Tooltip} from "@nextui-org/react";
-import { FooterData } from "@/utils/footer";
-import { addDoc, doc, getDocs, setDoc } from "@firebase/firestore";
-import { footerRef } from "@/utils/firebase";
-import { RenderFooter } from "@/components/Footer";
-import { locales } from "@/utils/langs";
 import { FiEdit, FiPlus, FiSave } from "react-icons/fi";
-import {Dropdown, DropdownTrigger, DropdownMenu, DropdownItem} from "@nextui-org/react";
 
 
-
-function UrlInput({document, lang, documents, setDocuments}: {document: {id: string, data: FooterData}, lang: (typeof locales)[number], documents: {id: string, data: FooterData}[], setDocuments: (documents: {id: string, data: FooterData}[]) => void}) {
-  return <Input 
-  value={document.data.url}
-  onValueChange={(value) => {
-    setDocuments(documents.map((doc) => {
-      if(doc.id === document.id) {
-        return {id: doc.id, data: {...doc.data, url: value}};
-      }
-      return doc;
-    }));
-  }}
-/>
+interface Document { 
+  id: string; 
+  data: FooterData 
 }
 
-function IconInput({document, lang, documents, setDocuments}: {document: {id: string, data: FooterData}, lang: (typeof locales)[number], documents: {id: string, data: FooterData}[], setDocuments: (documents: {id: string, data: FooterData}[]) => void}) {
-  return <Input 
-  value={document.data.icon}
-  onValueChange={(value) => {
-    setDocuments(documents.map((doc) => {
-      if(doc.id === document.id) {
-        return {id: doc.id, data: {...doc.data, icon: value}};
-      }
-      return doc;
-    }));
-  }}
-/>
+interface TableProps {
+  documents: Document[];
+  setDocuments: (documents: Document[]) => void
+}
+interface TablePropsWithDocument extends TableProps {
+  document: Document
 }
 
-function EditButton({document, lang, documents, setDocuments}: {document: {id: string, data: FooterData}, lang: (typeof locales)[number], documents: {id: string, data: FooterData}[], setDocuments: (documents: {id: string, data: FooterData}[]) => void}) {
-  return <Dropdown>
-  <DropdownTrigger>
-    <Button 
-      variant="bordered" 
-      color="secondary"
-      isIconOnly
-    >
-      <FiEdit size={24}/>
-    </Button>
-  </DropdownTrigger>
-  <DropdownMenu aria-label="Static Actions">
-    <DropdownItem key="move_up">Move up</DropdownItem>
-    <DropdownItem key="move_down">Move down</DropdownItem>
-    <DropdownItem key="delete" className="text-danger" color="danger">Delete</DropdownItem>
-  </DropdownMenu>
-</Dropdown>
-}
 
-function FooterLine({document, lang, documents, setDocuments}: {document: {id: string, data: FooterData}, lang: (typeof locales)[number], documents: {id: string, data: FooterData}[], setDocuments: (documents: {id: string, data: FooterData}[]) => void}) {
+function CreateTableLine({document, documents, setDocuments} : TablePropsWithDocument) {
+
+  const elements = [
+    (<RenderFooter data={document.data} lang="en"/>),
+    (<Input value={document.data.url} onValueChange={(value) => {
+      setDocuments(documents.map((doc) => {
+        if (doc.id === document.id) {
+          doc.data.url = value
+          return doc
+        }
+        return doc
+      }))
+    }}/>),
+    (<Input value={document.data.icon} onValueChange={(value) => {
+      setDocuments(documents.map((doc) => {
+        if (doc.id === document.id) {
+          doc.data.icon = value
+          return doc
+        }
+        return doc
+      }))
+    }}/>),
+  ]
+
   return (
-  <TableRow key={document.id}>
-    <TableCell>
-      <RenderFooter data={document.data} lang={lang}/>
-    </TableCell>
-    <TableCell>
-      <UrlInput document={document} lang={lang} documents={documents} setDocuments={setDocuments} />
-    </TableCell>
-    <TableCell>
-      <IconInput document={document} lang={lang} documents={documents} setDocuments={setDocuments} />
-    </TableCell>
-    <TableCell>
-      WIP
-    </TableCell>
-    <TableCell>
-      WIP
-    </TableCell>
-    <TableCell>
-      <EditButton document={document} lang={lang} documents={documents} setDocuments={setDocuments} />
-    </TableCell>
-</TableRow>)
+  <div className="flex flex-row gap-2 justify-start items-center h-full">
+      {elements.map((line, index) => (
+        <>
+          {line}
+          {index < elements.length - 1 && <Divider orientation="vertical" className="h-6"/>}
+        </>
+      ))}
+  </div>)
 }
 
-function FooterTable({lang}: {lang: (typeof locales)[number]}) {
-  const initData : FooterData = {
+
+function CreateTable({documents, setDocuments} : TableProps) {
+
+  const lines = documents.map((document) => {
+    return <CreateTableLine document={document} documents={documents} setDocuments={setDocuments}/>
+  })
+
+  return (
+  <Card>
+    <CardBody>
+      <div className="flex flex-col">
+          {lines.map((line, index) => (
+            <div key={index}>
+              {line}
+              {index < lines.length - 1 && <Divider />}
+            </div>
+          ))}
+      </div>
+    </CardBody>
+  </Card>
+  )
+}
+
+
+function FooterTable({ lang }: { lang: (typeof locales)[number] }) {
+  const initData: FooterData = {
     url: "",
     icon: "",
-    order: 0
-  }
-  const t = useTranslation()
+    order: 0,
+  };
+  const t = useTranslation();
   const [edit, setEdit] = useState(0);
-  const [documents, setDocuments] = useState<{id: string, data: FooterData}[]>([]);
+  const [progress, setProgress] = useState(0);
+  const [documents, setDocuments] = useState<
+    { id: string; data: FooterData }[]
+  >([]);
   useEffect(() => {
     getDocs(footerRef).then((q) => {
-      setDocuments(q.docs.map((doc) => ({id: doc.id, data: doc.data() as FooterData})));
+      setDocuments(
+        q.docs.map((doc) => ({ id: doc.id, data: doc.data() as FooterData })),
+      );
     });
-  }, []);
+  }, [edit]);
   const refresh = () => setEdit(edit + 1);
 
   const addFooter = () => {
-    const nexOrder = Math.max(...documents.map((doc) => doc.data.order ?? 0)) + 1;
-    addDoc(footerRef, {...initData, order: nexOrder}).then((doc) => {
-      refresh()
+    const nexOrder =
+      Math.max(...documents.map((doc) => doc.data.order ?? 0)) + 1;
+    addDoc(footerRef, { ...initData, order: nexOrder }).then((doc) => {
+      refresh();
     });
-  }
+  };
 
-  const saveToDatabase = () => {
-    documents.forEach((document) => {
-      setDoc(doc(footerRef, document.id), document.data).then(
-      )
-    })
-  }
+  const saveToDatabase = async () => {
+    onOpen();
+    setProgress(0);
+    for (let i = 0; i < documents.length; i++) {
+      await setDoc(doc(footerRef, documents[i].id), documents[i].data)
+      setProgress(i + 1);
+    }
+    onClose();
+  };
+  const { isOpen, onOpen, onOpenChange, onClose } = useDisclosure();
 
-  return (<>
-  <div className="flex flex-col gap-2">
-  <div className="flex flex-row justify-end w-full gap-2">
-    <Tooltip content={t["auth.account.footer.button_tooltip"]}>
-      <Button isIconOnly onClick={addFooter}>
-        <FiPlus size={30} />
-      </Button>
-    </Tooltip>
-    <Tooltip content={t["auth.account.footer.save_tooltip"]}>
-      <Button isIconOnly onClick={saveToDatabase} color="primary">
-        <FiSave size={30} />
-      </Button>
-    </Tooltip>
-  </div>
-  <Table aria-label="Example static collection table">
-    <TableHeader>
-      <TableColumn>RenderItem</TableColumn>
-      <TableColumn>Url</TableColumn>
-      <TableColumn>Icon url</TableColumn>
-      <TableColumn>Tooltip</TableColumn>
-      <TableColumn>Order</TableColumn>
-      <TableColumn>{""}</TableColumn>
-    </TableHeader>
-    <TableBody items={documents}>
-      {(document) => FooterLine({document: document, lang: lang, documents: documents, setDocuments: setDocuments})}
-    </TableBody>
-  </Table>
-  </div>
-  </>)
+  return (
+    <div className="flex flex-col gap-2">
+      <Modal
+        isOpen={isOpen}
+        onOpenChange={onOpenChange}
+        isDismissable={false}
+        isKeyboardDismissDisabled={true}
+        closeButton={<></>}
+      >
+        <ModalContent>
+          <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
+          <ModalBody>
+            Progress : {progress}, Total : {documents.length}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+      <div className="flex flex-col gap-2">
+        <div className="flex flex-row justify-end w-full gap-2">
+          <Tooltip content={t["auth.account.footer.button_tooltip"]}>
+            <Button isIconOnly onClick={addFooter}>
+              <FiPlus size={30} />
+            </Button>
+          </Tooltip>
+          <Tooltip content={t["auth.account.footer.save_tooltip"]}>
+            <Button isIconOnly onClick={saveToDatabase} color="primary">
+              <FiSave size={30} />
+            </Button>
+          </Tooltip>
+        </div>
+      </div>
+      <CreateTable documents={documents} setDocuments={setDocuments}/>
+    </div>
+  );
 }
-
-
-
 
 export default function Home({ params }: { params: HomeProps }) {
   const t = useTranslation();
@@ -174,8 +201,8 @@ export default function Home({ params }: { params: HomeProps }) {
   return (
     <>
       <IsUserLoggedIn fallback={<RedirectComponent />}>
-      <div className="flex flex-col justify-center items-center">
-        <div className="w-5/6 max-w-xl">
+        <div className="flex flex-col justify-center items-center">
+          <div className="w-5/6 max-w-xl">
             <div className="flex flex-col gap-2">
               <h1>{t["auth.account.title"]}</h1>
               <p>
@@ -187,7 +214,7 @@ export default function Home({ params }: { params: HomeProps }) {
                   {user?.uid}
                 </Snippet>
               </p>
-              <br/>
+              <br />
               <div>
                 <Button
                   onClick={() => {
@@ -200,7 +227,7 @@ export default function Home({ params }: { params: HomeProps }) {
                   {t["auth.account.signout"]}
                 </Button>
               </div>
-              <br/>
+              <br />
               <h2>{t["auth.account.change_map"]}</h2>
               <h4>{t["auth.account.change_map_small"]}</h4>
               <div className="flex flex-row gap-2">
@@ -212,12 +239,12 @@ export default function Home({ params }: { params: HomeProps }) {
               </div>
             </div>
             <h2>{t["auth.account.custom_footer"]}</h2>
+          </div>
+          <div className="w-5/6 max-w-3xl">
+            <FooterTable lang={params.lang} />
+          </div>
+          <br />
         </div>
-        <div className="w-5/6 max-w-3xl">
-          <FooterTable lang={params.lang}/>
-        </div>
-        <br/>
-      </div>
       </IsUserLoggedIn>
     </>
   );
